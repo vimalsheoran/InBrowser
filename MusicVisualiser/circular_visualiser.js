6db77f1e466e739423cea59f76658d21db15758e
vimalsheoran <vimalpsheoran@gmail.com>
2018-08-05 23:16:01 +0530

    Refactored Code and added a circular visualiser.

---
+++ MusicVisualiser/circular_visualiser.js
@@ -0,0 +1,77 @@
+const LEVEL_HEIGHT = 0.25;
+const LEVEL_THICKNESS = 15;
+const LEVEL_COLOR = 0;
+
+function Level(x, y, angle, radius){
+	this.x = x;
+	this.y = y;
+	this.updatedX = this.x;
+	this.updatedY = this.y;
+	this.angle = angle;
+	this.radius = radius;
+	this.levelHeight = LEVEL_HEIGHT;
+
+	this.drawLevel = function(){
+		strokeWeight(LEVEL_THICKNESS);
+		stroke(LEVEL_COLOR);
+		line(this.x, this.y, this.updatedX, this.updatedY);
+	}
+
+	this.updateLevelHeight = function(updateOffset){
+		this.updatedX = cos(radians(this.angle)) * (this.radius + updateOffset);
+		this.updatedY = sin(radians(this.angle)) * (this.radius + updateOffset);
+	}
+
+}
+
+const CANVAS_VERTICAL_OFFSET = 500;
+const NUMBER_OF_LEVELS = 40;
+const RADIUS = 120;
+const FREQ_OFFSET = 5;
+const AMPLITUDE_ATTENTUATION = 5;
+
+var levels = [];
+var fft;
+var mySound;
+var noOfLevels;
+var angularSpacing;
+var visualiserRadius;
+
+fft = new p5.FFT();
+mySound = new p5.AudioIn();
+
+function preload(){
+	mySound.connect(fft);
+	mySound.start();
+}
+
+function setup(){
+	smooth();
+	translate(windowWidth/2, windowHeight/2)
+	createCanvas(windowWidth, windowHeight + CANVAS_VERTICAL_OFFSET);
+	fft.setInput(mySound);
+	noOfLevels = NUMBER_OF_LEVELS;
+	angularSpacing = 360/noOfLevels;
+	visualiserRadius = RADIUS;
+	var angle = 0;
+	var i = 0;
+	while(angle < 360){
+		var xPos = cos(radians(angle)) * radius;
+		var yPos = sin(radians(angle)) * radius;
+		levels[i] = new Level(xPos, yPos, angle, radius)
+		angle += angularSpacing;
+		i++;
+	}
+}
+
+function draw(){
+	translate(windowWidth/2, windowHeight/2);
+	background(random(150, 200),0,0);
+	var spectrum = fft.analyze();
+	console.log(levels.length);
+	for(var i = 0; i < NUMBER_OF_LEVELS; i++){
+		levels[i].drawLevel();
+		levelHeight = map(spectrum[i*FREQ_OFFSET], 0, 255, 0, height);
+		levels[i].updateLevelHeight(levelHeight/AMPLITUDE_ATTENTUATION);
+	}
+}
--- MusicVisualiser/linear_visualiser.js
+++ MusicVisualiser/linear_visualiser.js
@@ -1,23 +1,28 @@
+const LEVEL_HEIGHT = 0.25;
+const LEVEL_THICKNESS = 15;
+const LEVEL_COLOR = 0;
+
 function Level(x){
 	this.x = x;
 	this.yBottom = height/2 + 100;
-	this.levelHeight = 0.25;
+	this.levelHeight = LEVEL_HEIGHT;
 
 	this.drawLevel = function(){
-		strokeWeight(15);
-		stroke(0);
-		console.log(this.levelHeight);
+		strokeWeight(LEVEL_THICKNESS);
+		stroke(LEVEL_COLOR);
 		line(this.x, this.yBottom, this.x, (this.yBottom - this.levelHeight));
 	}
 
 	this.updateLevelHeight = function(updateOffset){
 		this.levelHeight = updateOffset;
 	}
-
-	this.resetLevelHeight = function(){
-		this.levelHeight = 0.25;
-	}
 }
+
+const LEVEL_STARTING_X_POSITION = 100;
+const LEVEL_SPACING = 30;
+const NUMBER_OF_LEVELS = 40;
+const FREQ_OFFSET = 25;
+const AMPLITUDE_ATTENUATION = 5;
 
 var levels = [];
 var fft;
@@ -33,22 +38,19 @@ function preload(){
 
 function setup(){
 	smooth();
-	//mySound.setSource(0);
 	createCanvas(windowWidth, windowHeight + 500);
-	for(var i = 0; i < 40; i++){
-		levels[i] = new Level(100+(i*30));
+	for(var i = 0; i < NUMBER_OF_LEVELS; i++){
+		levels[i] = new Level(LEVEL_STARTING_X_POSITION+(i*LEVEL_SPACING));
 	}
-	//mySound.connect();
-	//mySound.start();
 	fft.setInput(mySound);
 }
 
 function draw(){
 	background(random(150, 200),0,0);
 	var spectrum = fft.analyze();
-	for(var i = 0; i < 40; i++){
+	for(var i = 0; i < NUMBER_OF_LEVELS; i++){
 		levels[i].drawLevel();
-		levelHeight = map(spectrum[i*15], 0, 255, height, 0)
-		levels[i].updateLevelHeight(levelHeight/2);
+		levelHeight = map(spectrum[i*FREQ_OFFSET], 0, 255, 0, height);
+		levels[i].updateLevelHeight(levelHeight/AMPLITUDE_ATTENUATION);
 	}
 }
